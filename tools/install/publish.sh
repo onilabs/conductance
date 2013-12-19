b5124a4d5db302874498413f82d72ed3460b7179
Tim Cuthbertson <tim@gfxmonk.net>
2013-12-19 13:50:24 +1100

    update installer publish script

--- tools/install/publish.sh
+++ tools/install/publish.sh
@@ -3,7 +3,8 @@ set -eu
 cd "$(dirname "$0")"
 [ "$#" = 1 ] || (echo "Usage: publish.sh DEST"; exit 1)
 dest="$1"
-../gup all
-. ./env.do.sh
+PATH="$PWD/../:$PATH"
+gup all
+. ./env.sh
 set -x
 cp -v --dereference $platform_bundles manifest*.json install.sh "$dest/"
